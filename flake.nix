3b36218bcb0fdb6a21f87d289a09d7db4ab99d7b
Charlie Wilkin <charlie@busyboredom.com>
2025-01-07 21:12:04 -0500

    Fix ixon on nixos wayland.

--- flake.nix
+++ flake.nix
@@ -27,15 +27,21 @@
           xorg.libXrandr
           alsa-lib
         ];
+      icon = pkgs.fetchurl {
+        url = "https://raw.githubusercontent.com/busyboredom/sedentary/refs/heads/main/static/sedentary.webp";
+        sha256 = "sha256-13RqUE7CFd08t0B5BbSM4InASaR1zc93/aM90QjiKrE=";
+      };
+      name = "sedentary";
     in {
       formatter = fmtr;
       defaultPackage = naersk-lib.buildPackage {
         src = ./.;
         doCheck = true;
-        pname = "sedentary";
+        pname = "${name}";
         nativeBuildInputs = [
-          pkgs.makeWrapper 
+          pkgs.makeWrapper
           pkgs.pkg-config
+          pkgs.imagemagick
         ];
         buildInputs = with pkgs; [
           xorg.libxcb
@@ -43,6 +49,10 @@
         ];
         postInstall = ''
           wrapProgram "$out/bin/sedentary" --prefix LD_LIBRARY_PATH : "${libPath}"
+          for i in 16 24 48 64 96 128 256 512; do
+            mkdir -p $out/share/icons/hicolor/''${i}x''${i}/apps
+            convert -background none -resize ''${i}x''${i} ${icon} $out/share/icons/hicolor/''${i}x''${i}/apps/${name}.png
+          done
         '';
       };
 
@@ -52,9 +62,9 @@
 
       devShell = with pkgs;
         mkShell {
-        nativeBuildInputs = [
-          pkgs.pkg-config
-        ];
+          nativeBuildInputs = [
+            pkgs.pkg-config
+          ];
           buildInputs = [
             cargo
             cargo-insta
